16d0c92d724baf859505aa8bf84c50d0c3ba5c15
Satish Rambhatla <ss.rambhatla@gmail.com>
2026-07-24 11:36:32 -0700

    Configure Astro for AWS Amplify SSR with astro-aws-amplify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import awsAmplify from 'astro-aws-amplify';
 
 export default defineConfig({
   site: 'https://calmandcourageoussleepers.com',
   output: 'hybrid',
-  adapter: node({ mode: 'standalone' }),
+  adapter: awsAmplify(),
   integrations: [tailwind()],
 });
